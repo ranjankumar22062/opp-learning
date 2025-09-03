29b7a1d85b04698ec956f745fe7f2008f57e4e1c
Ranjan Kumar <meb22062@gamil.com>
2025-09-03 23:10:45 +0530

    opps updated

--- opps_project.py
+++ opps_project.py
@@ -11,15 +11,17 @@ class chatbook:
                            2.press 2 to signin
                            3.press 3 to write a post
                            4.press 4 to message a friend
-                           5.press any other key to exit""")
+                           5.press any other key to exit
+                           
+                           """)
         if user_input =="1":
             self.signup()
         elif user_input=="2":
             self.signin()
         elif user_input=="3":
-            pass
+            self.my_post()
         elif user_input=="4":
-            pass
+            self.sendmsg()
         else:
             exit()
     def signup(self):
@@ -38,6 +40,32 @@ class chatbook:
             pwd= input("Enter your password")
             if self.username==uname and self.password==pwd:
                 print("you have sucessfully signin")
+                self.loggedin=True
+                
+            else:
+                print("please give correct crendentials")
+            print("\n")
+            self.menu()        
+    def my_post(self):
+        if self.loggedin==True:
+            txt=input("enter your message here")
+            print(f"following content has been posted -> {txt}")
+        else:
+            print("you need to signin first to post something")
+        print("\n")
+        self.menu()
+
+    def sendmsg(self):
+        if self.loggedin==True:
+            txt =input("enter your message here -->")
+            frnd=input ("whom to send -->")
+            print(f"Your message has been sent to {frnd}")
+        else:
+            print("you need to signin first to post something ...")
+        print("\n")
+        self.menu()            
+
+
             
 
 
