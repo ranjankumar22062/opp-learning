7973b5b1e7ca9d3e181e111974f075aa0d9f7f7c
Ranjan Kumar <meb22062@gamil.com>
2025-09-03 13:47:51 +0530

    updated

--- opps_project.py
+++ opps_project.py
@@ -13,13 +13,33 @@ class chatbook:
                            4.press 4 to message a friend
                            5.press any other key to exit""")
         if user_input =="1":
-            pass
+            self.signup()
         elif user_input=="2":
-            pass
+            self.signin()
         elif user_input=="3":
             pass
         elif user_input=="4":
             pass
         else:
             exit()
+    def signup(self):
+        email =input("enter your email address")
+        pwd=input("setup your password")
+        self.username=email
+        self.password=pwd
+        print("you have signup sucessfully")
+        print("\n")
+        self.menu()
+    def signin(self):
+        if self.username=='' and  self.password=='':
+            print("please signup first by pressing 1 ")
+        else:
+            uname= input("enter your email/username here")
+            pwd= input("Enter your password")
+            if self.username==uname and self.password==pwd:
+                print("you have sucessfully signin")
+            
+
+
+    
 obj=chatbook()
